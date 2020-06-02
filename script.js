ac2d9000ab804e961e89f45b2cd0badcee23875a
Brenden Forbis <brenden.forbis@outlook.com>
2020-06-02 18:47:28 -0400

    updated the GET request

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var incidentType = "theft"
 var proxSquare = "100"
 
 var queryUrl = "https://bikewise.org:443/api/v2/incidents?page=1&per_page=3&incident_type=" + incidentType + "&proximity=" + cityName +"&proximity_square="+ proxSquare   
-
+var queryUrlTwo = "https://api.meetup.com/recommended/groups?zip=10021&fields=join_info"
 $.ajax({
     url: queryUrl,
-    method: "GET"
+    method: "GET",
 }).then( function(response) {
     console.log(response);
 var incResponse = response.incidents;
@@ -14,4 +14,11 @@ incResponse.forEach( function(e) {
     console.log(e);
     incResponse.append("#");
 });
-})
+});
+
+$.ajax({
+    url: queryUrlTwo,
+    method: "GET",
+}).then(function(response) {
+    console.log(response)
+});
